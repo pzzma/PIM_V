167f8b1ac82eecc1bc8dae5ee7f33eb4b804d49d
Thiago Henrique dos Santos <72514176+Thiago-Henrique-Santos@users.noreply.github.com>
2023-04-17 23:02:37 -0300

    Iniciando o programa C# (o projeto C# é rodado dentro da pasta ProgramaConsole)

---
+++ ProgramaConsole/program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program {
+    class Program {
+        static void Main (string[] args) {
+            Console.Write("Insira um número: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num > 0) {
+                Console.WriteLine("{0} é um número positivo!", num);
+            } else if (num < 0) {
+                Console.WriteLine("{0} é um número negativo!", num);
+            } else {
+                Console.WriteLine("{0} podemos dizer que é neutro, nem positivo, nem negativo. O {1}divisor de águas{1} talvez?", num ,'"');
+            }
+        }
+    }
+}
